bd1ee25bb49e99a6f9dbcf8a8526b0c5404a459d
mattoki <matt@mattokimura.com>
2012-10-21 21:55:36 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vresume::Application.config.secret_token = 'dd68f00404cde98902dd578611225264a4e1f1dc7e70df4a129d195add2d5193cd2fcde9942e8bf2a8eea951a409d2e3d486d7e1c967ecfda4b8dd898231514f'
